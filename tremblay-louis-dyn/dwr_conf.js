31b16499494cd04b6141022877fddd386411bd55
sitrembl <simon.c.tremblay@gmail.com>
2022-03-09 23:05:58 -0500

    Update dwr_conf.js

--- tremblay-louis-dyn/dwr_conf.js
+++ tremblay-louis-dyn/dwr_conf.js
@@ -67,7 +67,7 @@ SVG_TREE_COLOR_SCHEME0 = ["#ffffff", "#ffffff"];
 SVG_TREE_COLOR_SCHEME1 = ["#ff3f00", "#ffaf0f", "#ffdf57", "#ffff6f", "#9fff9f", "#6fd7ff", "#4f97ff", "#e717ff", "#e71779", "#d2aa7c", "#bd9970"];
 SVG_TREE_COLOR_SCHEME2 = ["#e5bffc", "#bfbffc", "#bfdefc", "#b7dbc5", "#cef6d1"];
 FOOTER="";
-HEADER="";
+HEADER="<div class=\"grampsstylednote\">\n<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n<html>\n    <a href=\"..\\index.html\">Retour à l´index</a><br/>\n</html>\n</div>";
 BRAND_TITLE="";
 COPYRIGHT="";
 INDEX_SURNAMES_TYPE=true;
